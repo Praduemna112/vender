10a04a581d15895bd97768870fb27199bd61ab26
dpraduemna <dpraduemna12@gmail.com>
2025-04-02 00:37:27 +0530

    Fixed publish directory issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import React from "react";
 import "./App.css";
 import CounterPage from "./components/CounterPage/CounterPage";
 import Navbar from "./components/Navbar/Navbar";
