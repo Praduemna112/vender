84190a5450a684153e86b43ef6a6cf5efd209bed
dpraduemna <dpraduemna12@gmail.com>
2025-04-05 21:50:26 +0530

    Fixed publish directory issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";  // Import Link for navigation
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import CounterPage from "./components/CounterPage/CounterPage";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
@@ -16,14 +16,14 @@ function App() {
 
   return (
     <Router>
-      <ScrollToTop/>
+      <ScrollToTop />
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} /> {/* Home route */}
-          <Route path="/about" element={<AboutUs />} /> {/* Aboutus page */}
-          <Route path="/features" element={<Feacture />} /> {/* Feature page */}
-          <Route path="/contact" element={<Contact />} /> {/* Feature page */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/features" element={<Feacture />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
@@ -31,7 +31,6 @@ function App() {
   );
 }
 
-// Home component that shows all sections
 const Home = () => {
   return (
     <div>
